333e1622e772628836d3cb9bd8b8e1fc6345f026
Nebez Kassem <nabaxo@gmail.com>
2020-11-11 13:10:50 +0100

    Style Navbar to be responsive

--- src/App.tsx
+++ src/App.tsx
@@ -1,55 +1,64 @@
 import React, { useState } from 'react';
 import './App.css';
-import { Link, Redirect, Route, Router, Switch } from 'react-router-dom';
+import { NavLink, Redirect, Route, Router, Switch } from 'react-router-dom';
 import { Nav, Navbar } from 'react-bootstrap';
 import { createBrowserHistory } from 'history';
-// import firebase from 'firebase/app';
 
 import NewPost from './NewPost';
 import RoomList from './RoomList';
 import Settings from './Settings';
 import { User } from './types';
 import Login from './Login';
+import useWindowSize from './useWindowSize';
 
 const history = createBrowserHistory();
 
 function App() {
     const [user, setUser] = useState<User>();
     const [docId, setDocId] = useState<string>();
+    const { width } = useWindowSize();
 
+    const navBar = (
+        <Navbar
+            fixed={width > 576 ? 'top' : 'bottom'}
+            sticky={width > 576 ? 'top' : 'bottom'}
+            bg="dark"
+            variant="dark"
+            className="justify-content-center">
+            <Navbar.Brand className="text-white" >LFG</Navbar.Brand>
+            <Nav variant="pills">
+                <Nav.Item>
+                    <Nav.Link as={NavLink} to='/list' activeClassName="active">
+                        List
+                        </Nav.Link>
+                </Nav.Item>
+                <Nav.Item>
+                    <Nav.Link as={NavLink} to='/new-post' activeClassName="active">
+                        Room
+                        </Nav.Link>
+                </Nav.Item>
+                {user ?
+                    <Nav.Item>
+                        <Nav.Link as={NavLink} to='/settings' activeClassName="active">
+                            Settings
+                            </Nav.Link>
+                    </Nav.Item>
+                    :
+                    <Nav.Item>
+                        <Nav.Link as={NavLink} to='/login' activeClassName="active">
+                            Sign in
+                            </Nav.Link>
+                    </Nav.Item>
+                }
+            </Nav>
+        </Navbar>
+    );
+
+    // console.log(width);
     return (
         <Router history={history}>
             <div className="App">
-                <header className="App-header">
-                    <Navbar variant="dark" className="justify-content-center">
-                        <Navbar.Brand className="text-white" >LFG</Navbar.Brand>
-                        <Nav>
-                            <Nav.Item>
-                                <Nav.Link as={Link} to='/list'>
-                                    LFG List
-                                </Nav.Link>
-                            </Nav.Item>
-                            <Nav.Item>
-                                <Nav.Link as={Link} to='/new-post'>
-                                    Room
-                                </Nav.Link>
-                            </Nav.Item>
-                            {user ?
-                                <Nav.Item>
-                                    <Nav.Link as={Link} to='/settings'>
-                                        Settings
-                                    </Nav.Link>
-                                </Nav.Item>
-                                :
-                                <Nav.Item>
-                                    <Nav.Link as={Link} to='/login'>
-                                        Sign in
-                                    </Nav.Link>
-                                </Nav.Item>
-                            }
-                        </Nav>
-                    </Navbar>
-                </header>
+                {navBar}
                 <main>
                     <Switch>
                         <Route exact path='/'>
