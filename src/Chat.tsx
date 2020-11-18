8d868a76b6e4baa9d3d35e2da4d8fd8d5d9a1573
Nebez Kassem <nabaxo@gmail.com>
2020-11-18 16:41:46 +0100

    Polish Chat-component

--- src/Chat.tsx
+++ src/Chat.tsx
@@ -1,4 +1,4 @@
-import React, { ChangeEvent, useEffect, useState } from 'react';
+import React, { ChangeEvent, useEffect, useRef, useState } from 'react';
 import firebase from "firebase/app";
 import { ChatMessage } from "./types";
 import { Button, Form, Row, Col, Container, InputGroup } from 'react-bootstrap';
@@ -9,26 +9,26 @@ interface IProps {
   rid: string;
 }
 export default function Chat({ rid }: IProps) {
-  const collection = firebase.firestore().collection("rooms").doc(rid).collection("chat")
+  const bottomRef = useRef<HTMLDivElement>(null);
+  const collection = firebase.firestore().collection("rooms").doc(rid).collection("chat");
   const date = firebase.firestore.Timestamp.fromDate(new Date());
-  const username = firebase.auth().currentUser?.displayName
+  const username = firebase.auth().currentUser?.displayName;
   const [message, setMessage] = useState<ChatMessage>({
     mid: '',
     message: '',
     username: '',
     time: date
-  })
-  const [messages, setMessages] = useState<ChatMessage[]>([])
+  });
+  const [messages, setMessages] = useState<ChatMessage[]>([]);
 
   useEffect(() => {
-    const mCollection = firebase.firestore().collection("rooms").doc(rid).collection("chat").orderBy('time', 'asc')
+    const mCollection = firebase.firestore().collection("rooms").doc(rid).collection("chat").orderBy('time', 'asc');
     return mCollection.onSnapshot((snapshot) => {
       setMessages(snapshot.docs.map(d => ({
         ...d.data()
       })) as ChatMessage[]);
     });
-  }, [])
-
+  }, [rid]);
 
   function handleChange(event: ChangeEvent<any>) {
     const target = event.target;
@@ -39,37 +39,52 @@ export default function Chat({ rid }: IProps) {
         message: value,
         username: username,
         time: firebase.firestore.Timestamp.fromDate(new Date())
-      }
-      setMessage(message)
+      };
+      setMessage(message);
     }
   }
 
   function handleSubmit(event: React.FormEvent<HTMLFormElement>) {
     event.preventDefault();
 
-    if(message.message !== ''){
-      collection.doc(message.mid).set(message)
+    if (message.message !== '') {
+      collection.doc(message.mid).set(message);
+      setMessage({
+        mid: '',
+        message: '',
+        username: '',
+        time: date
+      });
+      scrollToBottom();
     }
+  }
+
+  function scrollToBottom() {
+    bottomRef.current?.scrollIntoView({
+      behavior: 'smooth',
+      block: 'start',
+    });
   }
 
   return (
     <Container className="mt-3">
       <Col className="chatbox">
-        {messages && messages.length && messages.map(m => 
+        {messages && messages.length && messages.map(m =>
           <Row className="d-flex justify-content-start ">
             <Col><span className="text-muted">{m.time.toDate().toLocaleTimeString()}</span> - <span className="text-primary">{m.username}:</span> {m.message}</Col>
           </Row>
-          )}
+        )}
+        <Row ref={bottomRef} className="chat-bottom"></Row>
       </Col>
       <Form onSubmit={handleSubmit}>
         <InputGroup>
           <Form.Control type="text" value={message.message}
             onChange={handleChange}
-            placeholder="messages" />
-        <Button
-          type="submit">Send
+            placeholder="Chat..." />
+          <Button
+            type="submit">Send
           </Button>
-          </InputGroup>
+        </InputGroup>
       </Form>
     </Container>
   );
